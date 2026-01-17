7e517ced8ae8b87c9038fc80bef88a4eabad03e3
khanhtimn <khanhlcbb@gmail.com>
2026-01-18 05:02:37 +0700

    deps: rand

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,9 @@
 use leptos::prelude::*;
-use leptos_meta::{provide_meta_context, MetaTags, Stylesheet, Title};
+use leptos_bevy_canvas::prelude::*;
+use leptos_meta::{MetaTags, Stylesheet, Title, provide_meta_context};
 use leptos_router::{
-    components::{Route, Router, Routes},
     StaticSegment,
+    components::{Route, Router, Routes},
 };
 
 pub fn shell(options: LeptosOptions) -> impl IntoView {
